c867e886fc3bf8a95e5cd913afbf0815014b6b16
[PT]GOODVIBE[PT] <83761813+tv4you2016@users.noreply.github.com>
2023-11-13 11:16:00 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='pyvesync',
-    version='2.1.10',
+    name='pyvesyncV2',
+    version='0.0.1',
     description='pyvesync is a library to manage Etekcity\
                  Devices, Cosori Air Fryers and Levoit Air \
                      Purifiers run on the VeSync app.',
