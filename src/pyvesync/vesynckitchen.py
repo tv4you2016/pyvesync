5a9fc2ffda9b93b783eaf1c03790a82bd87ffd33
[PT]GOODVIBE[PT] <tv4you2016@gmail.com>
2023-11-13 15:30:05 +0000

    Update vesynckitchen.py

--- src/pyvesync/vesynckitchen.py
+++ src/pyvesync/vesynckitchen.py
@@ -788,29 +788,10 @@ class VeSyncAirFryerCAF(VeSyncBaseDevice):
 
        
         if method_cmd['cookMode'].get('cookStatus') == 'endCook':
-            body['payload'] = str({'method': 'endCook', 'source': 'APP', 'data': {}})
+            body['payload'] = self.generate_payload_data('endCook', None, None, None)
         else:
-            body['payload'] = str({
-                'method': 'startCook',
-                'source': 'APP',
-                'data': {
-                    'accountId': self.manager.account_id,
-                    'hasPreheat': 0,
-                    'hasWarm': False,
-                    'mode': method_cmd['cookMode'].get('cookStatus'),
-                    'readyStart': True,
-                    'recipeId': 3,
-                    'recipeName': method_cmd['cookMode'].get('cookStatus'),
-                    'recipeType': 3,
-                    'startAct': {
-                        'cookSetTime': method_cmd['cookMode'].get('cookSetTime'),
-                        'cookTemp': method_cmd['cookMode'].get('cookSetTemp'),
-                        'preheatTemp': 0,
-                        'shakeTime': 0,
-                    },
-                    'tempUnit': 'c',
-                },
-            })
+            body['payload'] = self.generate_payload_data('startCook', method_cmd['cookMode'].get('cookStatus'), method_cmd['cookMode'].get('cookSetTime'), method_cmd['cookMode'].get('cookSetTemp'))
+
             if isinstance(body['payload'], dict):
                 data = body['payload'].get('data')
             else:
